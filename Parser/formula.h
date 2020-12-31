557a072399ccd931fd95e36c288d133bc303c468
jkjkil4 <52841865+jkjkil4@users.noreply.github.com>
2020-12-31 15:48:03 +0800

    删除了一些东西
    删除了因重写化学式而注释掉的原来写的和化学式有关的代码

--- Parser/formula.h
+++ Parser/formula.h
@@ -1,5 +1,5 @@
-#ifndef FORMULA_BASE_H
-#define FORMULA_BASE_H
+#ifndef FORMULA_H
+#define FORMULA_H
 
 #include <QVariant>
 #include <QList>
@@ -46,75 +46,5 @@ private:
     const QString& elementData() const;
     const QList<Formula>& groupData() const;
 };
-/*
-class Formula_Parent
-{
-public:
-    enum Type { Group, Element };
 
-    Formula_Parent() = default;
-    Formula_Parent(int count);
-    virtual ~Formula_Parent() = default;
-
-    //绘制化学式，xy为左下角
-    virtual void paint(QPainter &p, int &x, int y, bool useBrackets = false) const = 0;
-    //格式化为字符串
-    virtual QString format(bool useBrackets = false) const = 0;
-    //得到元素数量，不会自动清空mapElementCount
-    virtual void elementCount(QMap<QString, Frac> &mapElementCount, int mulCount = 1) const = 0;
-
-    int count = 1;
-    Frac elec;
-};
-
-class Formula_Group : public Formula_Parent
-{
-public:
-    Formula_Group(QString str, int count = 1, bool *ok = nullptr);
-    Formula_Group(const Formula_Group &other) = delete;
-    ~Formula_Group() override;
-
-    void paint(QPainter &p, int &x, int y, bool useBrackets = false) const override;
-    QString format(bool useBrackets = false) const override;
-    void elementCount(QMap<QString, Frac> &mapElementCount, int mulCount = 1) const override;
-
-    QList<Formula_Parent*> childs;
-};
-
-class Formula_Element : public Formula_Parent
-{
-public:
-    Formula_Element(const QString &str, bool *ok = nullptr);
-    ~Formula_Element() override = default;
-
-    void paint(QPainter &p, int &x, int y, bool = false) const override;
-    QString format(bool = false) const override;
-    void elementCount(QMap<QString, Frac> &mapElementCount, int mulCount = 1) const override;
-
-    QString element;
-};
-
-typedef Formula_Group Formula;
-
-inline void DrawFormula(QPainter &p, int x, int y, Formula *formula, int *pWidth = nullptr) {
-    int xStart = x;
-    formula->paint(p, x, y);
-    if(!formula->elec.mapPoly.isEmpty()) {
-        x += 5;
-        y -= QFontMetrics(p.font()).height();
-        j::SetPointSize(&p, qMax(1, p.font().pointSize() / 2));
-        QString elec = formula->elec.format(false, false);
-        bool isOne = Frac(formula->elec).sum(1).mapPoly.isEmpty() || Frac(formula->elec).sub(1).mapPoly.isEmpty();
-        if(elec[0] == '-') {
-            elec.remove(0, 1);
-            isOne ? elec = '-' : elec.append('-');
-        } else isOne ? elec = '+' : elec.append('+');
-
-        QRect rect;
-        j::DrawText(p, x, y, Qt::AlignLeft | Qt::AlignTop, elec, -1, -1, &rect);
-        x += rect.width();
-    }
-    SET_PTR(pWidth, x - xStart);
-}*/
-
-#endif // FORMULA_BASE_H
+#endif // FORMULA_H
