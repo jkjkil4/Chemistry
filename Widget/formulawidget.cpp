92709b1958d82226a89629d1a020e1d2337e71ce
jkjkil4 <52841865+jkjkil4@users.noreply.github.com>
2020-12-31 15:42:44 +0800

    关于绘制的一些修改

--- Widget/formulawidget.cpp
+++ Widget/formulawidget.cpp
@@ -8,18 +8,25 @@ void FormulaWidget::paintEvent(QPaintEvent *) {
     if(lReactants.isEmpty() && lProducts.isEmpty())
         return;
 
-    QPainter p(this);
-    j::SetPointSize(&p, 20);
-
-    int height = QFontMetrics(p.font()).height();
-    int x = 50;
-    int y = 50;
-    paint(p, lReactants, x, y);
-    x += 20;
     QRect rect;
-    j::DrawText(p, x, y + height, Qt::AlignLeft | Qt::AlignBottom, "=", -1, -1, &rect);
+    QPainter p(this);
+    j::SetPointSize(&p, 18);
+
+    int fmHeight = QFontMetrics(p.font()).height();
+    int x = 20;
+    int y = 20;
+
+    j::DrawText(p, x, y + fmHeight, Qt::AlignLeft | Qt::AlignBottom, "反应物:", -1, -1, &rect);
     x += rect.width() + 20;
-    paint(p, lProducts, x, y);
+    paint(p, lReactants, x, y);
+    //x += 20;
+    //j::DrawText(p, x, y + fmHeight, Qt::AlignLeft | Qt::AlignBottom, "=", -1, -1, &rect);
+    //x += rect.width() + 20;
+
+    x = 20;
+    j::DrawText(p, x, y + 2 * fmHeight + 10, Qt::AlignLeft | Qt::AlignBottom, "生成物:", -1, -1, &rect);
+    x += rect.width() + 20;
+    paint(p, lProducts, x, y + fmHeight + 10);
 }
 
 void FormulaWidget::clear() {
